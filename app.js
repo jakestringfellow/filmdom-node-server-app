18a47aa6a3b925eaa6a487b26d4b4f03746c091f
Jake Stringfellow <stringfellow.j@northeastern.edu>
2023-10-31 11:11:54 -0400

    Updated connection string

--- app.js
+++ app.js
@@ -17,12 +17,12 @@ import ReviewsController from './project/reviews-controller.js';
 
 //const CONNECTION_STRING = 'mongodb://127.0.0.1:27017/tuiter';//process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/tuiter';
 const CONNECTION_STRING = process.env.MONGO_URI;
-//const conn = mongoose.connect(CONNECTION_STRING);
-// mongoose.connect(CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true })
-//     .then(() => console.log('Connected to MongoDB'))
-//     .catch((error) => console.error("Could not connect to MongoDB", error));
-
 const conn = mongoose.connect(CONNECTION_STRING);
+mongoose.connect(CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((error) => console.error("Could not connect to MongoDB", error));
+
+// const conn = mongoose.connect(CONNECTION_STRING);
 
 //mongoose.connect("mongodb://127.0.0.1:27012/tuiter"); // connect to the tuiter database
 
